ac1b3749e22c6bf5827ef09e211ddbcca0748ed2
javidiazdom <javidiazdom@gmail.com>
2019-12-05 11:33:53 +0000

    Finished tabulation, not perfect yet

--- Implementation.py
+++ Implementation.py
@@ -21,7 +21,26 @@ def Memoization(n,coeff,rhs):
          for i in range (0,rhs+1):
             t += Memoization_aux(n-1,coeff,rhs-i*coeff[n])
          cache[key] = t
-      else:
-         print("found!")
       return cache[key]
-   return Memoization_aux(n,coeff,rhs),cache
+   return Memoization_aux(n,coeff,rhs)
+
+def Tabulation(n,coeff,rhs):
+   table = [[]]
+   def accessTable (x,y):
+      if (y < 0):
+         return 0
+      else:
+         return table[x][y]
+
+   for i in range (0,n+1):
+      table.append([])
+      for j in range (0,rhs+1):
+         table[i].append(0)
+      table[i][0] = 1
+   
+   for k in range (1, n+1):
+      for r in range (1, rhs+1):
+         for i in range (0,rhs+1):
+            table[k][r] += accessTable(k-1,r - i*coeff[k])
+   print(table)
+   return table[n][rhs]
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if (len(sys.argv) > 2):
     print("Error: Too many parameters. Needed: 1")
     exit(1)
 
-if (re.search("([1-9]*[a-z]?[+-])*[1-9]+[a-z]+=[1-9]+",sys.argv[1])):
+if (re.search("([1-9]+[a-z]?[+-])*[1-9]+[a-z]+=[1-9]+",sys.argv[1])):
     matches = re.findall("[1-9]*[a-z]",sys.argv[1])
     coeff = []
     coeffNames = []
@@ -15,5 +15,7 @@ if (re.search("([1-9]*[a-z]?[+-])*[1-9]+[a-z]+=[1-9]+",sys.argv[1])):
         coeffNames.append(re.findall("[a-z]+", match)[0])
     rhs=int(re.findall("[1-9]+",re.findall("=[1-9]+",sys.argv[1])[0])[0])
     print(imp.Memoization(len(coeff)-1,list(reversed(coeff)),rhs))
+    print(imp.BruteForce(len(coeff)-1,list(reversed(coeff)),rhs))
+    print(imp.Tabulation(len(coeff)-1,list(reversed(coeff)),rhs))
 else:
     print("Bad parameter: The ecuation has not a valid format")
