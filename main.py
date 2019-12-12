eadb190cb9294ac5c95991192aa27de7b6e19f11
Javier Diaz Domínguez <javidiazdom@gmail.com>
2019-12-12 14:58:58 +0000

    Added algorythm selection

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import time
 def missing():
    print("Missing desired algorythm")
 
-algorythm = missing
+algorythm = 0
+algorythms = [False,False,False,False,False,False]
 di, do, dt = (False,)*3
 coeffs = []
 rhss = []
@@ -18,7 +19,8 @@ def switch(i):
       "-do":displayOutput,
       "-dt":displayTime,
       "-f":getFromFile,
-      "-ec":inlineEcuation
+      "-ec":inlineEcuation,
+      "-o":selectAlgorythm
    }
    func=switcher.get(i,default)
    func()
@@ -26,6 +28,17 @@ def switch(i):
 def default():
    print("Invoke error: bad parameter \n Type Main.py -h for help")
    exit(-1)
+
+def selectAlgorythm():
+   sys.argv.pop(0)
+   global algorythm
+   options = sys.argv.pop(0)
+   algorythm = 1
+   for j in range(0,6):
+      if (re.search(str(j),options)):
+         algorythms[j] = True
+      
+
 
 def showHelp():
    file = open("./Help.txt","r")
@@ -88,68 +101,69 @@ if (di):
       print(i,"=",j)
    print("\n------\n")
 
-for coef,rhs in zip(coeffs,rhss):
-   start=time.time()
-   res = imp.BruteForce(len(coef)-1,coef,rhs)
-   tiempo=time.time() - start
-   print(coef,"=",rhs,"Tiempo de ejecución para Brute Force:",tiempo,"segundos")
-   print("------")
-   if (do):
-      print("Numero de soluciones:",res)
+if algorythm == 0 or algorythms[0]:
+   for coef,rhs in zip(coeffs,rhss):
+      start=time.time()
+      res = imp.BruteForce(len(coef)-1,coef,rhs)
+      tiempo=time.time() - start
+      print(coef,"=",rhs,"Tiempo de ejecución para Brute Force:",tiempo,"segundos")
+      print("------")
+      if (do):
+         print("Numero de soluciones:",res)
+   print("\n")
 
-print("\n")
+if algorythm == 0 or algorythms[1]:
+   for coef,rhs in zip(coeffs,rhss):
+      start=time.time()
+      res = imp.BruteForceIterator(len(coef)-1,coef,rhs)
+      tiempo=time.time() - start
+      print(coef,"=",rhs,"Tiempo de ejecución para Brute Force Iterativo:",tiempo,"segundos")
+      print("------")
+      if (do):
+         print("Numero de soluciones:",res)
+   print("\n")
 
-for coef,rhs in zip(coeffs,rhss):
-   start=time.time()
-   res = imp.BruteForceIterator(len(coef)-1,coef,rhs)
-   tiempo=time.time() - start
-   print(coef,"=",rhs,"Tiempo de ejecución para Brute Force Iterativo:",tiempo,"segundos")
-   print("------")
-   if (do):
-      print("Numero de soluciones:",res)
+if algorythm == 0 or algorythms[2]:
+   for coef,rhs in zip(coeffs,rhss):
+      start=time.time()
+      res = imp.Memoization(len(coef)-1,coef,rhs)
+      tiempo=time.time() - start
+      print(coef,"=",rhs,"Tiempo de ejecución para Memoization:",tiempo,"segundos")
+      print("------")
+      if (do):
+         print("Numero de soluciones:",res)
+   print("\n")
 
-print("\n")
+if algorythm == 0 or algorythms[3]:
+   for coef,rhs in zip(coeffs,rhss):
+      start=time.time()
+      res = imp.Tabulation(len(coef)-1,coef,rhs)
+      tiempo=time.time() - start
+      print(coef,"=",rhs,"Tiempo de ejecución para Tabulation:",tiempo,"segundos")
+      print("------")
+      if (do):
+         print("Numero de soluciones:",res)
+   print("\n")
 
-for coef,rhs in zip(coeffs,rhss):
-   start=time.time()
-   res = imp.Memoization(len(coef)-1,coef,rhs)
-   tiempo=time.time() - start
-   print(coef,"=",rhs,"Tiempo de ejecución para Memoization:",tiempo,"segundos")
-   print("------")
-   if (do):
-      print("Numero de soluciones:",res)
+if algorythm == 0 or algorythms[4]:
+   for coef,rhs in zip(coeffs,rhss):
+      start=time.time()
+      res = imp.BackTracking(len(coef)-1,coef,rhs)
+      tiempo=time.time() - start
+      print(coef,"=",rhs,"Tiempo de ejecución para Backtracking:",tiempo,"segundos")
+      print("------")
+      if (do):
+         print("Numero de soluciones:",res)
+   print("\n")
 
-print("\n")
-
-for coef,rhs in zip(coeffs,rhss):
-   start=time.time()
-   res = imp.Tabulation(len(coef)-1,coef,rhs)
-   tiempo=time.time() - start
-   print(coef,"=",rhs,"Tiempo de ejecución para Tabulation:",tiempo,"segundos")
-   print("------")
-   if (do):
-      print("Numero de soluciones:",res)
-   
-print("\n")
-
-for coef,rhs in zip(coeffs,rhss):
-   start=time.time()
-   res = imp.BackTracking(len(coef)-1,coef,rhs)
-   tiempo=time.time() - start
-   print(coef,"=",rhs,"Tiempo de ejecución para Backtracking:",tiempo,"segundos")
-   print("------")
-   if (do):
-      print("Numero de soluciones:",res)
-
-print("\n")
-
-for coef,rhs in zip(coeffs,rhss):
-   start=time.time()
-   res = imp.BackTrackingIterator(len(coef)-1,coef,rhs)
-   tiempo=time.time() - start
-   print(coef,"=",rhs,"Tiempo de ejecución para Backtracking iterativo:",tiempo,"segundos")
-   print("------")
-   if (do):
-      print("Numero de soluciones:",res)
+if algorythm == 0 or algorythms[5]:
+   for coef,rhs in zip(coeffs,rhss):
+      start=time.time()
+      res = imp.BackTrackingIterator(len(coef)-1,coef,rhs)
+      tiempo=time.time() - start
+      print(coef,"=",rhs,"Tiempo de ejecución para Backtracking iterativo:",tiempo,"segundos")
+      print("------")
+      if (do):
+         print("Numero de soluciones:",res)
 
 
